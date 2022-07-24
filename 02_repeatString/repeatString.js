b96d7795cc822dae72d85ace1a44292349527635
Adam Baker <adam_baker@live.co.uk>
2022-07-24 13:01:30 +0100

    First commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num > 0) {
+    return string.repeat(num);
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    return "";
+  }
 };
 
 // Do not edit below this line
